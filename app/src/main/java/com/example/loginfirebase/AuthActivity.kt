61e2e5a0c9848b364bb932b02ee4ec6ce0c21113
JsMares <JsMares0105@gmail.com>
2024-02-23 02:31:53 -0600

    Lógica para registrar usuarios con correo y contraseña. Aun no hay conexion con Firebase

--- app/src/main/java/com/example/loginfirebase/AuthActivity.kt
+++ app/src/main/java/com/example/loginfirebase/AuthActivity.kt
@@ -1,11 +1,75 @@
 package com.example.loginfirebase
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
+import com.google.firebase.auth.FirebaseAuth
 
 class AuthActivity : AppCompatActivity() {
+
+    lateinit var singUpButton: Button
+    lateinit var logInButton: Button
+    lateinit var emailEditText: EditText
+    private lateinit var passwordEditText: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
+
+        singUpButton = findViewById(R.id.logOutButton)
+        emailEditText = findViewById(R.id.emailEditText)
+        passwordEditText = findViewById(R.id.passwordEditText)
+        logInButton = findViewById(R.id.logInButton)
+
+        // Setup
+        setup()
+    }
+
+    private fun setup() {
+        singUpButton.setOnClickListener {
+            if (emailEditText.text.isNotEmpty() && passwordEditText.text.isNotEmpty()) {
+                FirebaseAuth.getInstance().createUserWithEmailAndPassword(emailEditText.text.toString(), passwordEditText.text.toString())
+                    .addOnCompleteListener {
+                        if (it.isSuccessful) {
+                            showHome(it.result?.user?.email ?: "", ProviderType.BASIC)
+                        } else {
+                            showAlert()
+                        }
+                }
+            }
+        }
+
+        logInButton.setOnClickListener {
+            if (emailEditText.text.isNotEmpty() && passwordEditText.text.isNotEmpty()) {
+                FirebaseAuth.getInstance().signInWithEmailAndPassword(emailEditText.text.toString(), passwordEditText.text.toString())
+                    .addOnCompleteListener {
+                        if (it.isSuccessful) {
+                            showHome(it.result?.user?.email ?: "", ProviderType.BASIC)
+                        } else {
+                            showAlert()
+                        }
+                    }
+            }
+        }
+    }
+
+    private fun showAlert() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Error")
+        builder.setMessage("Se ha producido un error autenticando al usuario")
+        builder.setPositiveButton("Aceptar", null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+    }
+
+    private fun showHome(email: String, provider: ProviderType) {
+        val homeIntent: Intent = Intent(this, HomeActivity::class.java).apply {
+            putExtra("email", email)
+            putExtra("provider", provider.name)
+        }
+        startActivity(homeIntent)
     }
 }
--- app/src/main/java/com/example/loginfirebase/HomeActivity.kt
+++ app/src/main/java/com/example/loginfirebase/HomeActivity.kt
@@ -2,10 +2,42 @@ package com.example.loginfirebase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.google.firebase.auth.FirebaseAuth
+
+enum class ProviderType {
+    BASIC
+}
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var emailTextView: TextView
+    lateinit var providerTextView: TextView
+    lateinit var logOutButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        emailTextView = findViewById(R.id.emailTextView)
+        providerTextView = findViewById(R.id.providerTextView)
+        logOutButton = findViewById(R.id.logOutButton)
+
+        // Setup
+        val bundle: Bundle? = intent.extras
+        val email: String? = bundle?.getString("email")
+        val provider: String? = bundle?.getString("provider")
+        setup(email ?: "", provider ?: "")
+    }
+
+    private fun setup(email: String, provider: String) {
+        emailTextView.text = email
+        providerTextView.text = provider
+
+        logOutButton.setOnClickListener {
+            FirebaseAuth.getInstance().signOut()
+            onBackPressed()
+        }
     }
 }
